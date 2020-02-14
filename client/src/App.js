4f654b23f7c3cf82c92189c9bbd79cd18740cdf5
richknicks <rlynch@gmail.com>
2020-02-14 11:52:26 -0500

    created class component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      players: []
+    }
+  }
+  componentDidMount(){
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(response=>{
+      console.log('players',response.data)
+      this.state({players: response.data})
+    })
+    .catch(error=>{
+      console.log('This is an error',error)
+    })
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
+  }
 }
 
 export default App;
